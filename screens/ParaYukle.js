248d248a4db321b9f31a1db7dadf9a1cd878dff7
emakyemis <emakyemis@gmail.com>
2020-12-28 17:48:28 +0300

    QR kod okuyucu tasarım hazırlandı.

---
+++ screens/ParaYukle.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`${data}₺ yüklendi.`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Kamera için izin vermelisiniz.</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>Kameraya erişim sağlanamıyor.</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button color="#b03060" title={'Ana Menüye Dön'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
