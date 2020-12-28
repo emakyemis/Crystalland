06587b1682bfb053032f93adb8af3ec02d30d039
mrvnc <merve.inc@ogr.sakarya.edu.tr>
2020-12-28 21:23:05 +0300

    Create Register.js
    Register screen eklendi.

---
+++ screens/Register.js
@@ -0,0 +1,159 @@
+
+import React from 'react';
+import {ImageBackground,StyleSheet,Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView,Image,SafeAreaView} from 'react-native';
+import { Button } from 'react-native-elements';
+import { useState } from 'react';
+import DatePicker from 'react-native-datepicker';
+
+const image={dolap:require('./assets/dönme_dolap.jpg'),};
+	const Register = () => {
+		const [date, setDate] = useState('09-10-2020');
+		return (
+		  <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+			  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			  <View style={styles.container}>
+				<ImageBackground source= {image.dolap} imageStyle={styles.image} style={styles.image_container}>
+
+					
+						<View style={styles.loginScreenContainer}>
+							<View style={styles.loginFormView}>
+							
+								<View style={styles.satir}><Text style={styles.baslik}>Ad:</Text><TextInput placeholder="Adınız" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} /></View>
+								<View style={styles.satir}><Text style={styles.baslik}>Soyad:</Text><TextInput placeholder="Soyadınız" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} /></View>
+								<View style={styles.satir}><Text style={styles.baslik}>E-Mail:</Text><TextInput keyboardType={'email-address'} placeholder="E-Mail" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} /></View>
+								<View style={styles.satir}><Text style={styles.baslik}>Şifre:</Text><TextInput placeholder="Şifre" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/></View>
+								<View style={styles.satir}><Text style={styles.baslik}>Tel No:</Text><TextInput keyboardType={'phone-pad'} placeholder="Telefon Numarası" placeholderColor="#c4c3cb" style={styles.loginFormTextInput}/></View>
+								<View style={styles.satir}><Text style={styles.baslik}>D. Tarihi:</Text>
+								
+									<DatePicker
+									style={styles.datePickerStyle}
+									date={date} //initial date from state
+									mode="date" //The enum of date, datetime and time
+									placeholder="select date"
+									format="DD-MM-YYYY"
+									minDate="01-01-2016"
+									maxDate="01-01-2019"
+									confirmBtnText="Confirm"
+									cancelBtnText="Cancel"
+									customStyles={{
+										dateIcon: {
+										//display: 'none',
+										position: 'absolute',
+										left: 0,
+										top: 4,
+										marginLeft: 0,
+										},
+										dateInput: {
+										marginLeft: 36,
+										},
+									}}
+									onDateChange={(date) => {
+										setDate(date);
+									}}
+									/>
+								</View>
+
+								<Button
+								buttonStyle={styles.loginButton}
+								onPress={() => this.onLoginPress()}
+								title="Giriş"
+								/>								
+							</View>							
+									</View>					
+						</ImageBackground>
+						
+						</View>
+						</TouchableWithoutFeedback>
+					</KeyboardAvoidingView>
+		); 
+};
+export default Register;
+const styles = StyleSheet.create({
+	containerView: {
+	  flex: 1,
+	},
+	loginScreenContainer: {
+	  flex: 1,
+	  marginTop: 250,
+	 
+  },
+  satir: {
+	  flexDirection: 'row',
+	  alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  },
+baslik:{
+	fontSize : 16,
+	fontWeight:'bold',
+	color:'#7E354D',
+},
+  image_container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+	resizeMode: "cover",
+	width:null,
+	height: null,
+  },
+	logoText: {
+	  fontSize: 40,
+	  fontWeight: "800",
+	  marginTop: 150,
+	  marginBottom: 30,
+	  textAlign: 'center',
+	},
+	loginFormView: {
+	  flex: 1,
+	  justifyContent: 'center',
+	  alignItems: 'center',
+	},
+	loginFormTextInput: {
+	  height: 43,
+	  fontSize: 14,
+	  borderRadius: 5,
+	  borderWidth: 1,
+	  borderColor: '#eaeaea',
+	  backgroundColor: '#fafafa',
+	  width: 400,
+	  margin: 20,
+	  textAlign:'center',
+	
+	},
+	loginButton: {
+	  backgroundColor: '#7E354D',
+	  borderRadius: 5,
+	  height: 45,
+	  width: 200,
+	  margin: 20,
+	},
+	kaydol: {
+		height: 45,
+		marginTop: 40,
+		color: '#fff',
+		fontSize:22,
+	  fontWeight:'bold',
+	},
+	container: {
+		flex: 1,
+		padding: 10,
+		justifyContent: 'center',
+		alignItems: 'center',
+	  },
+	  title: {
+		textAlign: 'center',
+		fontSize: 20,
+		fontWeight: 'bold',
+		padding: 20,
+	  },
+	  datePickerStyle: {
+		width: 200,
+		marginTop: 20,
+	  },
+  });
+
+ 
+  
